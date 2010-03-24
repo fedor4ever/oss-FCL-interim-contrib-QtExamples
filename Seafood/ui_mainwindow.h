db6cfc9403192efb76cbff744e7f4f2c4edc417f
John Kern <johnk@symbian.org>
2010-03-24 14:11:30 -0700

    forgot a file

--- Seafood/ui_mainwindow.h
+++ Seafood/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Wed Mar 24 10:03:10 2010
+** Created: Wed Mar 24 14:08:53 2010
 **      by: Qt User Interface Compiler version 4.6.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
