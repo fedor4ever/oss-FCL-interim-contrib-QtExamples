3c03029f4fa5101e739b7f65495a7505323383fc
John Kern <johnk@symbian.org>
2010-04-01 14:57:00 -0700

    display context menus

--- Seafood/main.cpp
+++ Seafood/main.cpp
@@ -1,11 +1,26 @@
 #include <QtGui/QApplication>
+#include <QWidget>
+
 #include "mainwindow.h"
+
+// By default, Actions is added to the Options menu. The
+// idea is to support context menus on non-touch mobiles.
+void disableContextMenu()
+{
+    QWidgetList widgets = QApplication::allWidgets();
+    QWidget* wid=0;
+    foreach(wid,widgets)
+    {
+        wid->setContextMenuPolicy(Qt::NoContextMenu);
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.createMenus();
+    disableContextMenu();
     w.setWindowTitle("Seafood");
     w.setStyleSheet("* { background-color:rgb(199,147,88); padding: 7px ; color:rgb(255,255,255)}");
 #if defined(Q_WS_S60) || defined(Q_WS_MAEMO_5)
