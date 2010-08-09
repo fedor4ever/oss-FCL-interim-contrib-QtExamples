56394695ccced84eeaf99a32885f91e66bf80b28
Sebastian Brannstrom <sebastianb@symbian.org>
2010-08-09 17:16:20 +0100

    CSV reader works, kind of. contacts.csv file added to project, must live in c:\ at the moment

--- contactengine/dbtools.cpp
+++ contactengine/dbtools.cpp
@@ -1,11 +1,14 @@
 #include <QtCore/QCoreApplication>
 #include <QtSql>
+#include <QFile>
 
 #include "database.h"
 #include "dbtools.h"
 
 bool DBTools::createTable(QString sqlStmt)
 {
+    qDebug() << "DB: create table statement: " << sqlStmt << endl;
+
     QSqlQuery query;
     return query.exec(sqlStmt);
 }
@@ -65,7 +68,7 @@ bool DBTools::createDB()
     // Find QSLite driver
     db = QSqlDatabase::addDatabase("QSQLITE");
 
-    db.setDatabaseName("C:\\workspace\\QtExamples\\contactengine\\contactengine.db");
+    db.setDatabaseName("C:\\contactengine.db");
 
     // Open databasee
     if(!db.open())
@@ -88,8 +91,9 @@ bool DBTools::createDB()
                 "title varchar(128),"
                 "site int,"
                 "department int,"
-                "floor int");
+                "floor int)");
 
+    qDebug() << "DB: create table rc=" << rc << endl;
     return rc;
 }
 
@@ -102,12 +106,77 @@ void DBTools::testDB()
 
 void DBTools::importCSV(QString fileName)
 {
+    QFile file(fileName);
+    QString line;
 
+    if (file.open(QFile::ReadOnly)) {
+        qDebug() << "importCSV file open OK" << endl;
+        char buf[1024];
+
+        while(!file.atEnd())
+        {
+            file.readLine(buf, sizeof(buf));
+            QString string(buf);
+            //qDebug() << string;
+            QStringList stringList = string.split(",");
+
+            //qDebug() << "count: " << stringList.count() << endl;
+            QStringListIterator iter(stringList);
+
+            QString strName;
+            QString strMobile;
+            QString strDeskphone;
+            QString strXtn;
+            QString strEmail;
+            QString strFloor;
+            QString strSkype;
+            QString strTwitter;
+            QString strTitle;
+            QString strSite;
+            QString strDepartment;
+
+            if (stringList.count() > 0)
+                strName = stringList[0];
+
+            if (stringList.count() > 1)
+                strMobile = stringList[1];
+
+            if (stringList.count() > 2)
+                strDeskphone = stringList[2];
+
+            if (stringList.count() > 3)
+                strXtn = stringList[3];
+
+            if (stringList.count() > 4)
+                strEmail = stringList[4];
+
+            if (stringList.count() > 5)
+                strSkype = stringList[5];
+
+            if (stringList.count() > 6)
+                strTwitter = stringList[6];
+
+            if (stringList.count() > 7)
+                strTitle = stringList[7];
+
+            if (stringList.count() > 8)
+                strSite = stringList[8];
+
+            if (stringList.count() > 9)
+                strDepartment = stringList[9];
+
+            if (stringList.count() > 10)
+                strFloor =stringList[10];
+
+            qDebug() << strName << strTitle << endl;
+      }
+    }
 }
 
 DBTools::DBTools(QObject *parent)
 {
     QObject();
+    createDB();
 }
 
 DBTools::~DBTools()
--- contactengine/main.cpp
+++ contactengine/main.cpp
@@ -1,10 +1,14 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include "dbtools.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    DBTools dbt;
+    dbt.importCSV("c:\\contacts.csv");
 
 #if defined(Q_WS_S60)
     w.showMaximized();
